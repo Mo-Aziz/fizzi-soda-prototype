525b4a68d7e554cd5c492a2a8ef09d4a67e99e72
Mo-Aziz <mohamed.aref32@gmail.com>
2024-09-20 11:52:09 -0500

    fixes

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -14,7 +14,7 @@ export default function Button({ buttonLink, buttonText, className }: Props) {
     <PrismicNextLink
       field={buttonLink}
       className={clsx(
-        "rounded-xl bg-orange-600 px-5 py-4 text-center text-xl font-bold uppercase tracking-wide text-white transition-colors duration-150 hover:bg-orange-700 md:text-2xl",
+        "z-10 rounded-xl bg-orange-600 px-5 py-4 text-center text-xl font-bold uppercase tracking-wide text-white transition-colors duration-150 hover:bg-orange-700 md:text-2xl",
         className,
       )}
     >
